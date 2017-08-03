a3c73048a59c0f0686b6d3046df72b90c591382c
Twan Koolen <koolen.twan@gmail.com>
2017-08-03 15:15:32 -0400

    Don't open a visualizer window during test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,5 @@ using RigidBodyTreeInspector
 using Base.Test
 
 val = Valkyrie()
-DrakeVisualizer.any_open_windows() || DrakeVisualizer.new_window()
 geometries = parse_urdf(ValkyrieRobot.urdfpath(), val.mechanism; package_path = [ValkyrieRobot.packagepath()]);
 @test length(geometries) == 81
