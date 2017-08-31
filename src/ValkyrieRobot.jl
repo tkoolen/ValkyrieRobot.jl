ba6d206286fb050170f2182e3255ceb191fb61d3
Twan Koolen <koolen.twan@gmail.com>
2017-08-31 02:18:21 -0400

    Download everything at build time instead of at runtime.

--- src/ValkyrieRobot.jl
+++ src/ValkyrieRobot.jl
@@ -11,83 +11,8 @@ export Valkyrie,
 include("bipedcontrolutil.jl")
 using .BipedControlUtil
 
-const module_tempdir = joinpath(Base.tempdir(), string(module_name(current_module())))
-const datadir = joinpath(module_tempdir, "Valkyrie")
-
-function cached_download(url::String, localFileName::String, cacheDir::String)
-    if !ispath(cacheDir)
-        mkpath(cacheDir)
-    end
-    fullCachePath = joinpath(cacheDir, localFileName)
-    if !isfile(fullCachePath)
-        download(url, fullCachePath)
-    end
-    fullCachePath
-end
-
-packagepath() = module_tempdir
-urdfpath() = joinpath(datadir, "valkyrie.urdf")
-
-const meshpaths = ["urdf/model/meshes/arms/aj1_left.obj";
-    "urdf/model/meshes/arms/aj1_right.obj";
-    "urdf/model/meshes/arms/aj2_left.obj";
-    "urdf/model/meshes/arms/aj2_right.obj";
-    "urdf/model/meshes/arms/aj3_left.obj";
-    "urdf/model/meshes/arms/aj3_right.obj";
-    "urdf/model/meshes/arms/aj4_left.obj";
-    "urdf/model/meshes/arms/aj4_right.obj";
-    "urdf/model/meshes/arms/aj5_left.obj";
-    "urdf/model/meshes/arms/aj5_right.obj";
-    "urdf/model/meshes/arms/aj6_left.obj";
-    "urdf/model/meshes/arms/aj6_right.obj";
-    "urdf/model/meshes/arms/palm_left.obj";
-    "urdf/model/meshes/arms/palm_right.obj";
-    "urdf/model/meshes/fingers/indexj1_left.obj";
-    "urdf/model/meshes/fingers/indexj1_right.obj";
-    "urdf/model/meshes/fingers/indexj2_left.obj";
-    "urdf/model/meshes/fingers/indexj2_right.obj";
-    "urdf/model/meshes/fingers/indexj3_left.obj";
-    "urdf/model/meshes/fingers/indexj3_right.obj";
-    "urdf/model/meshes/fingers/middlej1_left.obj";
-    "urdf/model/meshes/fingers/middlej1_right.obj";
-    "urdf/model/meshes/fingers/middlej2_left.obj";
-    "urdf/model/meshes/fingers/middlej2_right.obj";
-    "urdf/model/meshes/fingers/middlej3_left.obj";
-    "urdf/model/meshes/fingers/middlej3_right.obj";
-    "urdf/model/meshes/fingers/pinkyj1_left.obj";
-    "urdf/model/meshes/fingers/pinkyj1_right.obj";
-    "urdf/model/meshes/fingers/pinkyj2_left.obj";
-    "urdf/model/meshes/fingers/pinkyj2_right.obj";
-    "urdf/model/meshes/fingers/pinkyj3_left.obj";
-    "urdf/model/meshes/fingers/pinkyj3_right.obj";
-    "urdf/model/meshes/fingers/thumbj1_left.obj";
-    "urdf/model/meshes/fingers/thumbj1_right.obj";
-    "urdf/model/meshes/fingers/thumbj2_left.obj";
-    "urdf/model/meshes/fingers/thumbj2_right.obj";
-    "urdf/model/meshes/fingers/thumbj3_left.obj";
-    "urdf/model/meshes/fingers/thumbj3_right.obj";
-    "urdf/model/meshes/fingers/thumbj4_left.obj";
-    "urdf/model/meshes/fingers/thumbj4_right.obj";
-    "urdf/model/meshes/head/head_multisense.obj";
-    "urdf/model/meshes/head/head_multisense_no_visor.obj";
-    "urdf/model/meshes/head/multisense_hokuyo.obj";
-    "urdf/model/meshes/head/neckj1.obj";
-    "urdf/model/meshes/head/neckj2.obj";
-    "urdf/model/meshes/legs/foot.obj";
-    "urdf/model/meshes/legs/lj1_left.obj";
-    "urdf/model/meshes/legs/lj1_right.obj";
-    "urdf/model/meshes/legs/lj2_left.obj";
-    "urdf/model/meshes/legs/lj2_right.obj";
-    "urdf/model/meshes/legs/lj3_left.obj";
-    "urdf/model/meshes/legs/lj3_right.obj";
-    "urdf/model/meshes/legs/lj4_left.obj";
-    "urdf/model/meshes/legs/lj4_right.obj";
-    "urdf/model/meshes/legs/lj5.obj";
-    "urdf/model/meshes/multisense/head_camera.obj";
-    "urdf/model/meshes/pelvis/pelvis.obj";
-    "urdf/model/meshes/torso/torso.obj";
-    "urdf/model/meshes/torso/torsopitch.obj";
-    "urdf/model/meshes/torso/torsoyaw.obj"]
+packagepath() = Pkg.dir("ValkyrieRobot", "deps")
+urdfpath() = joinpath(packagepath(), "Valkyrie", "valkyrie.urdf")
 
 function default_contact_model()
     SoftContactModel(hunt_crossley_hertz(k = 500e3), ViscoelasticCoulombModel(0.8, 20e3, 100.))
@@ -105,15 +30,7 @@ type Valkyrie{T}
     floatingjoint::Joint{T}
 
     function (::Type{Valkyrie{T}}){T}(; contactmodel = default_contact_model())
-        valkyrie_examples_url = "https://raw.githubusercontent.com/RobotLocomotion/drake/6e3ca768cbaabf15d0f2bed0fb5bd703fa022aa5/drake/examples/Valkyrie"
-        urdf_url = valkyrie_examples_url * "/urdf/urdf/valkyrie_A_sim_drake_one_neck_dof_wide_ankle_rom.urdf"
-        urdfdir, urdffile = splitdir(urdfpath())
-        urdf = cached_download(urdf_url, urdffile, urdfdir)
-        for meshpath in meshpaths
-            meshdir, meshfilename = splitdir(meshpath)
-            cached_download(joinpath(valkyrie_examples_url, meshpath), meshfilename, joinpath(datadir, meshdir))
-        end
-        mechanism = RigidBodyDynamics.parse_urdf(T, urdf)
+        mechanism = RigidBodyDynamics.parse_urdf(T, urdfpath())
 
         # pelvis
         pelvis = findbody(mechanism, "pelvis")
